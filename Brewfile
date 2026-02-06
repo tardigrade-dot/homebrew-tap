e8d899f3beb0ba0cc110c5cace8336d264d6a4e5
tardigrade-dot <you@example.com>
2026-02-06 15:46:06 +0800

    init

---
+++ Brewfile
@@ -0,0 +1,16 @@
+# 指定 Tap 仓库
+tap "homebrew/bundle"
+tap "tardigrade-dot/tap"
+
+# 安装命令行工具 (Formulae)
+brew "git"
+brew "node"
+brew "aphelios-cli" # 你刚创建的工具
+
+# 安装 GUI 软件 (Casks)
+cask "visual-studio-code"
+cask "google-chrome"
+cask "EasyFind"
+
+# 甚至可以安装 Mac App Store 的软件 (需要先安装 mas)
+# mas "Xcode", id: 497799835
---
+++ Formula/ollama.rb
@@ -0,0 +1,80 @@
+class Ollama < Formula
+  desc "Create, run, and share large language models (LLMs)"
+  homepage "https://ollama.com/"
+  url "https://github.com/ollama/ollama.git",
+      tag:      "v0.15.4",
+      revision: "6a7c3f188e679b1849e4618754b25dc9c19541d7"
+  license "MIT"
+  head "https://github.com/ollama/ollama.git", branch: "main"
+
+  # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
+  # labeled as "pre-release" on GitHub before the version is released, so it's
+  # necessary to use the `GithubLatest` strategy.
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+
+  on_macos do
+    on_arm do
+      depends_on "mlx-c" => :no_linkage
+    end
+  end
+
+  conflicts_with cask: "ollama-app"
+
+  def install
+    # Remove ui app directory
+    rm_r("app")
+
+    ENV["CGO_ENABLED"] = "1"
+
+    # Silence tens of thousands of SDK warnings
+    ENV["SDKROOT"] = MacOS.sdk_path if OS.mac?
+
+    ldflags = %W[
+      -s -w
+      -X github.com/ollama/ollama/version.Version=#{version}
+      -X github.com/ollama/ollama/server.mode=release
+    ]
+
+    mlx_args = []
+
+    # Flags for MLX (Apple silicon only)
+    if OS.mac? && Hardware::CPU.arm?
+      mlx_rpath = rpath(target: Formula["mlx-c"].opt_lib)
+      ldflags << "-extldflags '-Wl,-rpath,#{mlx_rpath}'"
+      mlx_args << "-tags=mlx"
+    end
+
+    system "go", "generate", "./..."
+    system "go", "build", *mlx_args, *std_go_args(ldflags:)
+  end
+
+  service do
+    run [opt_bin/"ollama", "serve"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/ollama.log"
+    error_log_path var/"log/ollama.log"
+    environment_variables OLLAMA_FLASH_ATTENTION: "1",
+                          OLLAMA_KV_CACHE_TYPE:   "q8_0"
+  end
+
+  test do
+    port = free_port
+    ENV["OLLAMA_HOST"] = "localhost:#{port}"
+
+    pid = spawn bin/"ollama", "serve"
+    begin
+      sleep 3
+      assert_match "Ollama is running", shell_output("curl -s localhost:#{port}")
+    ensure
+      Process.kill "TERM", pid
+      Process.wait pid
+    end
+  end
+end
