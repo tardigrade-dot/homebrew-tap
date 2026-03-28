e2a13fc2dac6765f0def9663c7b358fa7f1575fb
tardigrade-dot <you@example.com>
2026-03-28 13:43:52 +0800

    feat: rename aphelios-cli formula to aphelios_tool and add aphelios-slint formula

---
+++ Formula/aphelios-slint.rb
@@ -0,0 +1,22 @@
+class ApheliosCli < Formula
+  desc "Rust 编写的工具描述"
+  homepage "https://github.com/tardigrade-dot/aphelios_cli"
+  version "0.1.0"
+
+  # 针对不同架构定义下载地址
+  if OS.mac?
+
+    url "https://github.com/tardigrade-dot/aphelios_cli/releases/download/v0.0.1/aphelios_slint-x86_64-apple-darwin.tar.gz"
+    sha256 "d820d340d491a301d2afbc7623633a60d79e8697c58806fdd627cc7f2c3021a4"
+  end
+
+  def install
+    # 这就是 Formula 里的安装命令
+    # 将解压出来的二进制文件安装到 brew 的 bin 目录
+    bin.install "aphelios_slint"
+  end
+
+  test do
+    system "#{bin}/aphelios_slint", "--version"
+  end
+end
--- Formula/aphelios_tool.rb
+++ Formula/aphelios_tool.rb
@@ -6,17 +6,17 @@ class ApheliosCli < Formula
   # 针对不同架构定义下载地址
   if OS.mac?
 
-    url "https://github.com/tardigrade-dot/aphelios_cli/releases/download/v0.0.1/aphelios_cli-x86_64-apple-darwin.tar.gz"
-    sha256 "241d5a7e11bb66ed988862bc51fd6d72da72deab2489269a6583449ab50d374a"
+    url "https://github.com/tardigrade-dot/aphelios_cli/releases/download/v0.0.1/aphelios_tool-x86_64-apple-darwin.tar.gz"
+    sha256 "a785d006915617d51ad5e0b819b1413ce211ef43b79028a5a7a506923b3de082"
   end
 
   def install
     # 这就是 Formula 里的安装命令
     # 将解压出来的二进制文件安装到 brew 的 bin 目录
-    bin.install "aphelios_cli"
+    bin.install "aphelios_tool"
   end
 
   test do
-    system "#{bin}/aphelios_cli", "--version"
+    system "#{bin}/aphelios_tool", "--version"
   end
 end
